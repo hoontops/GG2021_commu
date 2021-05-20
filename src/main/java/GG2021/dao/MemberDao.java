d2f7f0ee952a07c204baa8d9f67a171f47ddb339
jiseong0612 <hth5545@gmail.com>
2021-05-20 15:08:01 +0900

    마스터- sqlsession 추가

--- src/main/java/GG2021/dao/MemberDao.java
+++ src/main/java/GG2021/dao/MemberDao.java
@@ -1,8 +1,14 @@
 package GG2021.dao;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDao {
-
+	@Autowired
+	private SqlSession sqlSession;
+	
+	
+	
 }
