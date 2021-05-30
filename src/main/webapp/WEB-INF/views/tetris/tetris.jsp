e9a3a6bd3906f21b18e25f9739baaf38159527f0
jh1016 <76502622+jh1016@users.noreply.github.com>
2021-05-31 00:56:08 +0900

    테트리스 다른걸로 바꿈

--- src/main/webapp/WEB-INF/views/tetris/tetris.jsp
+++ src/main/webapp/WEB-INF/views/tetris/tetris.jsp
@@ -9,198 +9,44 @@
 <meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="./css/style.css" />
 <title>TETRIS</title>
-<script>
-import BLOCKS from "./blocks.js"
-
-//DOM
-const playground = document.querySelector(".playground > ul");
-const gameText = document.querySelector(".game-text");
-const scoreDisplay = document.querySelector(".score");
-const restartButton = document.querySelector(".game-text > button");
-//Setting
-const GAME_ROWS = 20;
-const GAME_COLS = 10;
-
-//variables
-let score=0;  //점수
-let duration = 500;   //블럭이 떨어지는 시간
-let dowInterval;
-let tempMovingItem;
-
-
-
-const movingItem = {
-type: "" ,
-direction: 3,
-top: 0,
-left:0,
+<script type="text/javascript" src="./js/tetris.js"></script>
+<style>
+.wrapper-container{
+	margin-left: 35%;
+	margin-top: 30px;
 }
-
-init()
-
-//functions
-function init(){
- tempMovingItem = {...movingItem};
- for(let i=0; i<GAME_ROWS; i++){
-   prependNewLine()
- }
- generateNewBlock()
-}
-
-
-function prependNewLine(){
-const li = document.createElement("li");
-const ul = document.createElement("ul");
-for(let j=0; j<GAME_COLS; j++){
-    const matrix = document.createElement("li");
-    ul.prepend(matrix)
-}
-li.prepend(ul)
-playground.prepend(li)
-}
-function renderBlocks(moveType=""){
-const { type, direction, top, left } = tempMovingItem;
-const movingBlocks = document.querySelectorAll(".moving")
-movingBlocks.forEach(moving=>{
-    moving.classList.remove(type, "moving");
-})
-BLOCKS[type][direction].some(block => {
-  const x= block[0] + left;
-  const y = block[1] + top;
-  const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
-  const isAvailable = checkEmpth(target);
-  if(isAvailable){
-    target.classList.add(type, "moving")
-  }else{
-    tempMovingItem = {...movingItem }
-    if(moveType === 'retry'){
-      clearInterval(dowInterval)
-      showGameoverTex()
-    }
-     setTimeout(()=>{
-       renderBlocks('retry');
-       if(moveType === "top"){
-         seizeBlock()
-       }
-     },0)
-    // renderBlocks()
-    return true;
-  }
-})
-movingItem.left = left;
-movingItem.top = top;
-movingItem.direction = direction;   
-}
-function seizeBlock(){
-const movingBlocks = document.querySelectorAll(".moving")
-movingBlocks.forEach(moving => {
-    moving.classList.remove("moving");
-    moving.classList.add("seized");
-})
-checkMatch()
-}
-function checkMatch(){
-
-const childNodes = playground.childNodes;
-childNodes.forEach(child=>{
-  let matched = true;
-  child.children[0].childNodes.forEach(li=>{
-     if(!li.classList.contains("seized")){
-      matched = false;
-     }
-  })
-  if(matched){
-    child.remove();
-    prependNewLine();
-    score++;
-    scoreDisplay.innerText = score;
-  }
-})
-
-generateNewBlock()
-}
-
-function generateNewBlock(){
-clearInterval(dowInterval);
-dowInterval = setInterval(() => {
-  moveBlock('top',1)
-}, duration )
-
-
-const blockArray = Object.entries(BLOCKS);
-const randomIndex =Math.floor( Math.random() * blockArray.length)
-movingItem.type = blockArray[randomIndex][0]
-movingItem.top = 0;
-movingItem.left = 3;
-movingItem.direction = 0;
-tempMovingItem = { ...movingItem };
-renderBlocks();
-}
-
-function checkEmpth(target){
-if(!target || target.classList.contains("seized")){
-  return false;
-}
-return true;
-}
-
-function moveBlock(moveType, amount){
-tempMovingItem[moveType] += amount;
-renderBlocks(moveType)
-}
-function chageDirection(){
-const direction = tempMovingItem.direction;
-direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction +=1;
-renderBlocks();
-}
-function dropBlock(){
-  clearInterval(dowInterval);
-  dowInterval = setInterval(() => {
-    moveBlock("top",1)
-  }, 10);
-}
-function showGameoverTex(){
-gameText.style.display = "flex"
-}
-
-//event handling
-document.addEventListener("keydown", e => {
-switch(e.keyCode){
-  case 39:
-    moveBlock("left",1);
-    break;
-    case 37:
-      moveBlock("left",-1);
-    case 40:
-      moveBlock("top", 1);
-      break;
-    case 38:
-      chageDirection();
-      break;
-    case 32:
-      dropBlock();
-      break;
-  default:
-    break;
-}
-
-})
-
-
-</script>
+</style>
 </head>
 <body>
-    <div class="wrapper">
-        <div class="game-text">
-            게임종료
-        <button>다시 시작</button>
-        </div>
-        <div class="score">0</div>
-        <div class="playground">
-            <ul></ul>
-        </div>
+    <div class="wrapper-container">
+        <span class="tetris-container">
+            <canvas id="stage" width="250px" height="500px" style="background-color:black;">
+            </canvas>
+            <span class="tetris-panel-container">
+              <p>Next:</p>
+                <canvas id="next" width="150px" height="150px" style="background-color:black;">
+                </canvas>
+              <p>LINES:<span id="lines">0</span></p>
+              <p><span id="message"></span></p>
+              <div class="tetris-panel-container-padding"></div>
+              <table class="tetris-button-panel">
+                <tr>
+                  <td></td>
+                  <td id="tetris-rotate-button" class="tetris-button">↻</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="tetris-move-left-button"class="tetris-button">←</td>
+                  <td id="tetris-fall-button"class="tetris-button">↓</td>
+                  <td id="tetris-move-right-button"class="tetris-button">→</td>
+                </tr>
+              </table>
+            </span>
+        </span>
     </div>
-    <script src="./js/tetris.js" type="module"></script>
-
+    <script>
+      var tetris = new Tetris();
+      tetris.startGame();
+    </script>
 </body>
 </html>
--- src/main/webapp/js/tetris.js
+++ src/main/webapp/js/tetris.js
@@ -1,181 +1,318 @@
-import BLOCKS from "./blocks.js"
+class Tetris {
+    constructor() {
+        this.stageWidth = 10;
+        this.stageHeight = 20;
+        this.stageCanvas = document.getElementById("stage");
+        this.nextCanvas = document.getElementById("next");
+        let cellWidth = this.stageCanvas.width / this.stageWidth;
+        let cellHeight = this.stageCanvas.height / this.stageHeight;
+        this.cellSize = cellWidth < cellHeight ? cellWidth : cellHeight;
+        this.stageLeftPadding = (this.stageCanvas.width - this.cellSize * this.stageWidth) / 2;
+        this.stageTopPadding = (this.stageCanvas.height - this.cellSize * this.stageHeight) / 2;
+        this.blocks = this.createBlocks();
+        this.deletedLines = 0;
 
-//DOM
-const playground = document.querySelector(".playground > ul");
-const gameText = document.querySelector(".game-text");
-const scoreDisplay = document.querySelector(".score");
-const restartButton = document.querySelector(".game-text > button");
-//Setting
-const GAME_ROWS = 20;
-const GAME_COLS = 10;
+        window.onkeydown = (e) => {
+            if (e.keyCode === 37) {
+                this.moveLeft();
+            } else if (e.keyCode === 38) {
+                this.rotate();
+            } else if (e.keyCode === 39) {
+                this.moveRight();
+            } else if (e.keyCode === 40) {
+                this.fall();
+            }
+        }
 
-//variables
-let score=0;  //점수
-let duration = 500;   //블럭이 떨어지는 시간
-let dowInterval;
-let tempMovingItem;
-
-
-
-const movingItem = {
-  type: "" ,
-  direction: 3,
-  top: 0,
-  left:0,
-}
-
-init()
-
-//functions
-function init(){
-   tempMovingItem = {...movingItem};
-   for(let i=0; i<GAME_ROWS; i++){
-     prependNewLine()
-   }
-   generateNewBlock()
-}
-
-
-function prependNewLine(){
-  const li = document.createElement("li");
-  const ul = document.createElement("ul");
-  for(let j=0; j<GAME_COLS; j++){
-      const matrix = document.createElement("li");
-      ul.prepend(matrix)
-  }
-  li.prepend(ul)
-  playground.prepend(li)
-}
-function renderBlocks(moveType=""){
-  const { type, direction, top, left } = tempMovingItem;
-  const movingBlocks = document.querySelectorAll(".moving")
-  movingBlocks.forEach(moving=>{
-      moving.classList.remove(type, "moving");
-  })
-  BLOCKS[type][direction].some(block => {
-    const x= block[0] + left;
-    const y = block[1] + top;
-    const target = playground.childNodes[y] ? playground.childNodes[y].childNodes[0].childNodes[x] : null;
-    const isAvailable = checkEmpth(target);
-    if(isAvailable){
-      target.classList.add(type, "moving")
-    }else{
-      tempMovingItem = {...movingItem }
-      if(moveType === 'retry'){
-        clearInterval(dowInterval)
-        showGameoverTex()
-      }
-       setTimeout(()=>{
-         renderBlocks('retry');
-         if(moveType === "top"){
-           seizeBlock()
-         }
-       },0)
-      // renderBlocks()
-      return true;
+        document.getElementById("tetris-move-left-button").onmousedown = (e) => {
+            this.moveLeft();
+        }
+        document.getElementById("tetris-rotate-button").onmousedown = (e) => {
+            this.rotate();
+        }
+        document.getElementById("tetris-move-right-button").onmousedown = (e) => {
+            this.moveRight();
+        }
+        document.getElementById("tetris-fall-button").onmousedown = (e) => {
+            this.fall();
+        }
     }
-  })
-  movingItem.left = left;
-  movingItem.top = top;
-  movingItem.direction = direction;   
-}
-function seizeBlock(){
-  const movingBlocks = document.querySelectorAll(".moving")
-  movingBlocks.forEach(moving => {
-      moving.classList.remove("moving");
-      moving.classList.add("seized");
-  })
-  checkMatch()
-}
-function checkMatch(){
 
-  const childNodes = playground.childNodes;
-  childNodes.forEach(child=>{
-    let matched = true;
-    child.children[0].childNodes.forEach(li=>{
-       if(!li.classList.contains("seized")){
-        matched = false;
-       }
-    })
-    if(matched){
-      child.remove();
-      prependNewLine();
-      score++;
-      scoreDisplay.innerText = score;
+    createBlocks() {
+        let blocks = [
+            {
+                shape: [[[-1, 0], [0, 0], [1, 0], [2, 0]],
+                        [[0, -1], [0, 0], [0, 1], [0, 2]],
+                        [[-1, 0], [0, 0], [1, 0], [2, 0]],
+                        [[0, -1], [0, 0], [0, 1], [0, 2]]],
+                color: "rgb(0, 255, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 128, 128)"
+            },
+            {
+                shape: [[[0, 0], [1, 0], [0, 1], [1, 1]],
+                        [[0, 0], [1, 0], [0, 1], [1, 1]],
+                        [[0, 0], [1, 0], [0, 1], [1, 1]],
+                        [[0, 0], [1, 0], [0, 1], [1, 1]]],
+                color: "rgb(255, 255, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 128, 0)"
+            },
+            {
+                shape: [[[0, 0], [1, 0], [-1, 1], [0, 1]],
+                        [[-1, -1], [-1, 0], [0, 0], [0, 1]],
+                        [[0, 0], [1, 0], [-1, 1], [0, 1]],
+                        [[-1, -1], [-1, 0], [0, 0], [0, 1]]],
+                color: "rgb(0, 255, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 128, 0)"
+            },
+            {
+                shape: [[[-1, 0], [0, 0], [0, 1], [1, 1]],
+                        [[0, -1], [-1, 0], [0, 0], [-1, 1]],
+                        [[-1, 0], [0, 0], [0, 1], [1, 1]],
+                        [[0, -1], [-1, 0], [0, 0], [-1, 1]]],
+                color: "rgb(255, 0, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 0, 0)"
+            },
+            {
+                shape: [[[-1, -1], [-1, 0], [0, 0], [1, 0]],
+                        [[0, -1], [1, -1], [0, 0], [0, 1]],
+                        [[-1, 0], [0, 0], [1, 0], [1, 1]],
+                        [[0, -1], [0, 0], [-1, 1], [0, 1]]],
+                color: "rgb(0, 0, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(0, 0, 128)"
+            },
+            {
+                shape: [[[1, -1], [-1, 0], [0, 0], [1, 0]],
+                        [[0, -1], [0, 0], [0, 1], [1, 1]],
+                        [[-1, 0], [0, 0], [1, 0], [-1, 1]],
+                        [[-1, -1], [0, -1], [0, 0], [0, 1]]],
+                color: "rgb(255, 165, 0)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 82, 0)"
+            },
+            {
+                shape: [[[0, -1], [-1, 0], [0, 0], [1, 0]],
+                        [[0, -1], [0, 0], [1, 0], [0, 1]],
+                        [[-1, 0], [0, 0], [1, 0], [0, 1]],
+                        [[0, -1], [-1, 0], [0, 0], [0, 1]]],
+                color: "rgb(255, 0, 255)",
+                highlight: "rgb(255, 255, 255)",
+                shadow: "rgb(128, 0, 128)"
+            }
+        ];
+        return blocks;
     }
-  })
 
-  generateNewBlock()
+    drawBlock(x, y, type, angle, canvas) {
+        let context = canvas.getContext("2d");
+        let block = this.blocks[type];
+        for (let i = 0; i < block.shape[angle].length; i++) {
+            this.drawCell(context,
+                     x + (block.shape[angle][i][0] * this.cellSize),
+                     y + (block.shape[angle][i][1] * this.cellSize),
+                     this.cellSize,
+                     type);
+        }
+    }
+drawCell(context, cellX, cellY, cellSize, type) {
+        let block = this.blocks[type];
+        let adjustedX = cellX + 0.5;
+        let adjustedY = cellY + 0.5;
+        let adjustedSize = cellSize - 1;
+        context.fillStyle = block.color;
+        context.fillRect(adjustedX, adjustedY, adjustedSize, adjustedSize);
+        context.strokeStyle = block.highlight;
+        context.beginPath();
+        context.moveTo(adjustedX, adjustedY + adjustedSize);
+        context.lineTo(adjustedX, adjustedY);
+        context.lineTo(adjustedX + adjustedSize, adjustedY);
+        context.stroke();
+        context.strokeStyle = block.shadow;
+        context.beginPath();
+        context.moveTo(adjustedX, adjustedY + adjustedSize);
+        context.lineTo(adjustedX + adjustedSize, adjustedY + adjustedSize);
+        context.lineTo(adjustedX + adjustedSize, adjustedY);
+        context.stroke();
+    }
+
+    startGame() {
+        let virtualStage = new Array(this.stageWidth);
+        for (let i = 0; i < this.stageWidth; i++) {
+            virtualStage[i] = new Array(this.stageHeight).fill(null);
+        }
+        this.virtualStage = virtualStage;
+        this.currentBlock = null;
+        this.nextBlock = this.getRandomBlock();
+        this.mainLoop();
+    }
+
+    mainLoop() {
+        if (this.currentBlock == null) {
+            if (!this.createNewBlock()) {
+                return;
+            }
+        } else {
+            this.fallBlock();
+        }
+        this.drawStage();
+        if (this.currentBlock != null) {
+            this.drawBlock(this.stageLeftPadding + this.blockX * this.cellSize,
+                this.stageTopPadding + this.blockY * this.cellSize,
+                this.currentBlock, this.blockAngle, this.stageCanvas);
+        }
+        setTimeout(this.mainLoop.bind(this), 500);
+    }
+
+    createNewBlock() {
+        this.currentBlock = this.nextBlock;
+        this.nextBlock = this.getRandomBlock();
+        this.blockX = Math.floor(this.stageWidth / 2 - 2);
+        this.blockY = 0;
+        this.blockAngle = 0;
+        this.drawNextBlock();
+        if (!this.checkBlockMove(this.blockX, this.blockY, this.currentBlock, this.blockAngle)) {
+            let messageElem = document.getElementById("message");
+            messageElem.innerText = "GAME OVER";
+            return false;
+        }
+        return true;
+    }
+
+    drawNextBlock() {
+        this.clear(this.nextCanvas);
+        this.drawBlock(this.cellSize * 2, this.cellSize, this.nextBlock,
+            0, this.nextCanvas);
+    }
+
+    getRandomBlock() {
+        return  Math.floor(Math.random() * 7);
+    }
+
+    fallBlock() {
+        if (this.checkBlockMove(this.blockX, this.blockY + 1, this.currentBlock, this.blockAngle)) {
+            this.blockY++;
+        } else {
+            this.fixBlock(this.blockX, this.blockY, this.currentBlock, this.blockAngle);
+            this.currentBlock = null;
+        }
+    }
+
+    checkBlockMove(x, y, type, angle) {
+        for (let i = 0; i < this.blocks[type].shape[angle].length; i++) {
+            let cellX = x + this.blocks[type].shape[angle][i][0];
+            let cellY = y + this.blocks[type].shape[angle][i][1];
+            if (cellX < 0 || cellX > this.stageWidth - 1) {
+                return false;
+            }
+            if (cellY > this.stageHeight - 1) {
+                return false;
+            }
+            if (this.virtualStage[cellX][cellY] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    fixBlock(x, y, type, angle) {
+        for (let i = 0; i < this.blocks[type].shape[angle].length; i++) {
+            let cellX = x + this.blocks[type].shape[angle][i][0];
+            let cellY = y + this.blocks[type].shape[angle][i][1];
+            if (cellY >= 0) {
+                this.virtualStage[cellX][cellY] = type;
+            }
+        }
+        for (let y = this.stageHeight - 1; y >= 0; ) {
+            let filled = true;
+            for (let x = 0; x < this.stageWidth; x++) {
+                if (this.virtualStage[x][y] == null) {
+                    filled = false;
+                    break;
+                }
+            }
+            if (filled) {
+                for (let y2 = y; y2 > 0; y2--) {
+                    for (let x = 0; x < this.stageWidth; x++) {
+                        this.virtualStage[x][y2] = this.virtualStage[x][y2 - 1];
+                    }
+                }
+                for (let x = 0; x < this.stageWidth; x++) {
+                    this.virtualStage[x][0] = null;
+                }
+            let linesElem = document.getElementById("lines");
+                this.deletedLines++;
+                linesElem.innerText = "" + this.deletedLines;
+            } else {
+                y--;
+            }
+        }
+    }
+drawStage() {
+        this.clear(this.stageCanvas);
+
+        let context = this.stageCanvas.getContext("2d");
+        for (let x = 0; x < this.virtualStage.length; x++) {
+            for (let y = 0; y < this.virtualStage[x].length; y++) {
+                if (this.virtualStage[x][y] != null) {
+                    this.drawCell(context,
+                        this.stageLeftPadding + (x * this.cellSize),
+                        this.stageTopPadding + (y * this.cellSize),
+                        this.cellSize,
+                        this.virtualStage[x][y]);
+                }
+            }
+        }
+    }
+
+    moveLeft() {
+        if (this.checkBlockMove(this.blockX - 1, this.blockY, this.currentBlock, this.blockAngle)) {
+            this.blockX--;
+            this.refreshStage();
+        }
+    }
+
+    moveRight() {
+        if (this.checkBlockMove(this.blockX + 1, this.blockY, this.currentBlock, this.blockAngle)) {
+            this.blockX++;
+            this.refreshStage();
+        }
+    }
+
+    rotate() {
+        let newAngle;
+        if (this.blockAngle < 3) {
+            newAngle = this.blockAngle + 1;
+        } else {
+            newAngle = 0;
+        }
+        if (this.checkBlockMove(this.blockX, this.blockY, this.currentBlock, newAngle)) {
+            this.blockAngle = newAngle;
+            this.refreshStage();
+        }
+    }
+
+    fall() {
+        while (this.checkBlockMove(this.blockX, this.blockY + 1, this.currentBlock, this.blockAngle)) {
+            this.blockY++;
+            this.refreshStage();
+        }
+    }
+
+    refreshStage() {
+      this.clear(this.stageCanvas);
+      this.drawStage();
+      this.drawBlock(this.stageLeftPadding + this.blockX * this.cellSize,
+                this.stageTopPadding + this.blockY * this.cellSize,
+                this.currentBlock, this.blockAngle, this.stageCanvas);
+    }
+
+    clear(canvas) {
+        let context = canvas.getContext("2d");
+        context.fillStyle = "rgb(0, 0, 0)";
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    }
 }
-
-function generateNewBlock(){
-  clearInterval(dowInterval);
-  dowInterval = setInterval(() => {
-    moveBlock('top',1)
-  }, duration )
-
-
-  const blockArray = Object.entries(BLOCKS);
-  const randomIndex =Math.floor( Math.random() * blockArray.length)
-  movingItem.type = blockArray[randomIndex][0]
-  movingItem.top = 0;
-  movingItem.left = 3;
-  movingItem.direction = 0;
-  tempMovingItem = { ...movingItem };
-  renderBlocks();
-}
-
-function checkEmpth(target){
-  if(!target || target.classList.contains("seized")){
-    return false;
-  }
-  return true;
-}
-
-function moveBlock(moveType, amount){
-  tempMovingItem[moveType] += amount;
-  renderBlocks(moveType)
-}
-function chageDirection(){
-  const direction = tempMovingItem.direction;
-  direction === 3 ? tempMovingItem.direction = 0 : tempMovingItem.direction +=1;
-  renderBlocks();
-}
-function dropBlock(){
-    clearInterval(dowInterval);
-    dowInterval = setInterval(() => {
-      moveBlock("top",1)
-    }, 10);
-}
-function showGameoverTex(){
-  gameText.style.display = "flex"
-}
-
-//event handling
-document.addEventListener("keydown", e => {
-  switch(e.keyCode){
-    case 39:
-      moveBlock("left",1);
-      break;
-      case 37:
-        moveBlock("left",-1);
-      case 40:
-        moveBlock("top", 1);
-        break;
-      case 38:
-        chageDirection();
-        break;
-      case 32:
-        dropBlock();
-        break;
-    default:
-      break;
-  }
-  
-})
-
-restartButton.addEventListener("click",()=>{
-  playground.innerHTML="";
-  gameText.style.display="none";
-	init()
-})
